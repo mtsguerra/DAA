c8ee758c8faf5140e07a502df3b156607ded5f42
mtsguerra7 <mtsguerra9@gmail.com>
2025-03-11 12:59:04 +0000

    Updating

---
+++ lista1/exB.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+class exB{
+
+    private static final Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int numberOfRoutes = input.nextInt();
+        int numberOfElementsInTheGroup = input.nextInt();
+        int start = input.nextInt();
+        int end = input.nextInt();
+        input.nextLine();
+        for (int i=0;i<numberOfRoutes;i++){
+            
+        }
+    }
+}
