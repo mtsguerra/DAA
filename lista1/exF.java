c36067674fd9f465cb6399d129742609e252f790
mtsguerra7 <mtsguerra9@gmail.com>
2025-03-11 23:00:46 +0000

    up

---
+++ lista1/exF.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+class exF{
+
+    private static final Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int numberOfTeams = input.nextInt();
+        int numberOfSubmissions = input.nextInt();
+        int minTime = input.nextInt();
+        int initialTime = input.nextInt();
+        int teamWantsToKnow = input.nextInt();
+        input.nextLine();
+    }
+}
--- lista1/exG.java
+++ lista1/exG.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 class exG{
 
@@ -6,13 +6,66 @@ class exG{
 
     public static void main(String[] args) {
 
-        int sizeNumber =  input.nextInt();
-        input.nextLine();
+        int numberSize =  input.nextInt();
         int totalTries = input.nextInt();
         input.nextLine();
 
-        String confirmed = "X".repeat(sizeNumber);
-        int[] numbersWeKnow =  new int[sizeNumber];
+        Set<Integer> confirmedNumbers = new HashSet<>();
+        Set<Integer> inPosition = new HashSet<>();
+        List<HashSet<Integer>> setsArray = new ArrayList<>();
+        for (int i = 0; i < numberSize; i++) {
+            setsArray.add(new HashSet<>());
+            for (int j = 0; j < 10; j++) {
+                setsArray.get(i).add(j);
+            }
+        }
+        for (int i=0;i<totalTries;i++){
+            String numberString = input.nextLine();
+            String review = input.nextLine();
+            for (int j=0;j<numberSize;j++){
+                char currentReview = review.charAt(j);
+                char currentDigit = numberString.charAt(j);
+                int numNow = currentDigit - '0';
+                if (currentReview == 'X'){;
+                    for (int k=0;k<numberSize;k++){
+                        setsArray.get(k).remove(numNow);
+                    }
+                }
+                else if (currentReview == '-'){
+                    confirmedNumbers.add(numNow);
+                    setsArray.get(j).remove(numNow);
+                }
+                else if (Character.isDigit(currentReview)){
+                    confirmedNumbers.add(numNow);
+                    inPosition.add(numNow);
+                    HashSet<Integer> singleDigit = new HashSet<>();
+                    singleDigit.add(currentReview -'0');
+                    setsArray.set(j,singleDigit);
+                }
+            }
+        }
 
+        for (int i=0;i<numberSize;i++) {
+            if (confirmedNumbers.size() == numberSize && setsArray.get(i).size()>1) {
+                setsArray.get(i).removeAll(inPosition);
+            }
+            setsArray.get(i).removeIf(n -> !confirmedNumbers.contains(n));
+        }
+
+        StringBuilder sb = new StringBuilder();
+        boolean found = true;
+        for (int i=0;i<numberSize;i++){
+            if (setsArray.get(i).size() == 1) {
+                sb.append(setsArray.get(i).iterator().next());
+            }
+            else {
+                System.out.print("AINDA NAO SEI");
+                found = false;
+                break;
+            }
+        }
+        if (found){
+            System.out.println("RESPOSTA " + sb.toString());
+        }
     }
 }
