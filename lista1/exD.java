8319409a6e5f9e2464056f3f64d2185f1c77d156
mtsguerra7 <mtsguerra9@gmail.com>
2025-03-15 20:02:15 +0000

    up

--- lista1/exD.java
+++ lista1/exD.java
@@ -1,3 +1,64 @@
+import java.util.*;
+
 class exD {
 
+    private static final Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+
+        int stadingPeople = 0;
+        int avaiableSeats = 0;
+
+        int numberOfChairs = input.nextInt();
+        input.nextLine();
+
+        HashMap<Integer, Integer> chairs = new HashMap<>();
+        for (int i=0;i<numberOfChairs;i++){
+            int typeOfChair = input.nextInt();
+            input.nextLine();
+            int numberOfThisType = input.nextInt();
+            input.nextLine();
+            if (numberOfThisType>=1){
+                chairs.put(typeOfChair,numberOfThisType);
+                avaiableSeats+=numberOfThisType;
+            }
+        }
+
+        int numberOfPeople = input.nextInt();
+        input.nextLine();
+
+        Queue<List<Integer>> people = new LinkedList<>();
+        for (int i=0;i<numberOfPeople;i++){
+            int numberOfOptions = input.nextInt();
+            input.nextLine();
+            List<Integer> crr = new ArrayList<>();
+            for (int j=0;j<numberOfOptions;j++){
+                crr.add(input.nextInt());
+                input.nextLine();
+            }
+            if (crr.isEmpty()) stadingPeople++;
+            else{
+                people.add(crr);
+            }
+        }
+
+        while (!people.isEmpty()){
+            List<Integer> currentPref = people.poll();
+            boolean seated = false;
+            for (int n : currentPref){
+                if (chairs.containsKey(n)){
+                    int remaining = chairs.get(n)-1;
+                    if (remaining == 0) chairs.remove(n);
+                    else chairs.put(n,remaining);
+                    seated = true;
+                    avaiableSeats--;
+                    break;
+                }
+            }
+            if (!seated) stadingPeople++;
+        }
+
+        System.out.println(stadingPeople);
+        System.out.println(avaiableSeats);
+    }
 }
--- lista1/exF.java
+++ lista1/exF.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 class exF{
@@ -5,11 +7,95 @@ class exF{
     private static final Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
+
         int numberOfTeams = input.nextInt();
-        int numberOfSubmissions = input.nextInt();
-        int minTime = input.nextInt();
-        int initialTime = input.nextInt();
+        int nn = input.nextInt();
+        int delay = input.nextInt();
+        int blackoutStart = input.nextInt();
         int teamWantsToKnow = input.nextInt();
         input.nextLine();
+
+        // HashMap<Team, HashMap<Problem, int[isRight, time, wrongSubmissions, lastSubmission]>>
+        HashMap<Integer,
+                HashMap<Integer, int[]>> teams = new HashMap<>();
+
+        for (int i = 0; i< nn; i++){
+
+            int team = input.nextInt();
+            int time = input.nextInt();
+            int problem = input.nextInt();
+            int isRight = input.nextInt();
+            input.nextLine();
+
+            teams.putIfAbsent(team, new HashMap<>());
+            HashMap<Integer, int[]> currentProblem = teams.get(team);
+
+            currentProblem.putIfAbsent(problem, new int[]{0,0,0,0});
+            int[] problemInfo = currentProblem.get(problem);
+
+            if (isRight==1){
+                problemInfo[0]=1;
+                problemInfo[1]=time;
+            }
+            else{
+                problemInfo[2]++;
+            }
+            problemInfo[3]=time;
+        }
+
+        int blackoutSubs = input.nextInt();
+        input.nextLine();
+
+        int vTotalTime =0;
+
+        for (int i = 0; i<blackoutSubs; i++){
+            if (i==blackoutSubs-1){
+                int lastAccepted = input.nextInt();
+                input.nextLine();
+                vTotalTime+=lastAccepted;
+                break;
+            }
+            vTotalTime+=20*60;
+            input.nextLine();
+        }
+
+        HashMap<Integer, int[]> vTeam = teams.get(teamWantsToKnow);
+
+        for (Map.Entry<Integer, int[]> entry : vTeam.entrySet()) {
+            int[] problemInfo = entry.getValue();
+            vTotalTime += problemInfo[1] + 20 * problemInfo[2] * 60;
+        }
+
+        int minProblemsSolved = vTotalTime / blackoutStart;
+
+        for (int i=1;i<=numberOfTeams;i++){
+            if (!teams.containsKey(i) || i==teamWantsToKnow) continue;
+            HashMap<Integer, int[]> current = teams.get(i);
+            if (current.size() < minProblemsSolved) continue;
+            int t = getT(current, delay, minProblemsSolved, blackoutStart);
+            if (t < vTotalTime){
+                System.out.println("Nao sabemos");
+                return;
+            }
+        }
+        System.out.println("Vencemos");
+    }
+
+    private static int getT(HashMap<Integer, int[]> current, int delay, int minProblemsSolved, int blackoutStart) {
+        int solved =0;
+        int totalTime=0;
+        int lastSubmissionTime = 0;
+        for (int[] problemInfo : current.values()) {
+            if (problemInfo[0]==1) solved++;
+            totalTime += problemInfo[1] + 20 * problemInfo[2] * 60;
+            lastSubmissionTime = Math.max(lastSubmissionTime, problemInfo[3]);
+        }
+        int currentTime = Math.max(lastSubmissionTime+delay, blackoutStart);
+        while (solved < 7 && currentTime <= 14400) {
+            totalTime+=currentTime;
+            currentTime+=delay;
+            solved++;
+        }
+        return totalTime;
     }
 }
