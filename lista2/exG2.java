d65ec72a54f5bd3d2e8ccc228dc64da245c0fa3b
mtsguerra7 <mtsguerra9@gmail.com>
2025-03-20 22:34:18 +0000

    first try

---
+++ lista2/exG2.java
@@ -0,0 +1,73 @@
+import java.util.*;
+
+class exG2{
+
+    private static final Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int numberOfNodes = input.nextInt();
+        int numberOfActivities = input.nextInt();
+        int k = input.nextInt();
+        int totalTime = 0;
+
+        int[] nEdges = new int[numberOfNodes+1];
+        HashMap<Integer, List<int[]>> branches = new HashMap<>();
+        for (int i = 1; i <= numberOfNodes; i++) {
+            branches.put(i, new ArrayList<>());
+        }
+
+        for (int i=0;i<numberOfActivities;i++){
+            int node1 = input.nextInt();
+            int node2 = input.nextInt();
+            int weight = input.nextInt();
+            totalTime += weight;
+            branches.get(node1).add(new int[]{node2,weight});
+            nEdges[node2]++;
+        }
+        int n=2;
+        n+=2;
+        KahnAlgorithm(branches,nEdges,numberOfActivities,numberOfNodes, totalTime, k);
+    }
+
+    private static void KahnAlgorithm (HashMap<Integer, List<int[]>> connections,
+                                      int [] nEdges, int numberOfActivities, int numberOfNodes,
+                                       int totalTime, int k){
+        Queue<Integer> queue = new LinkedList<>();
+        int [] topologicalOrderWithTime = new int[numberOfNodes];
+        int index =0;
+        for (int zz =0;zz<numberOfActivities;zz++){
+            for (int i=1;i<=numberOfNodes;i++){
+                if (nEdges[i] == 0){
+                    queue.add(i);
+                    nEdges[i] = -1;
+                }
+            }
+            boolean found = false;
+            if (queue.contains(k)){
+                found = true;
+                queue.remove(k);
+            }
+            while (!queue.isEmpty()){
+                int current = queue.poll();
+                topologicalOrderWithTime[index] = current;
+                index++;
+                for (int[] neighbor : connections.get(current)){
+                    nEdges[neighbor[0]]--;
+                    totalTime -= neighbor[1];
+                }
+            }
+            if (found){
+                int remainingRelations = 0;
+                for (int n : nEdges){
+                    if (n > 0){
+                        remainingRelations+=n;
+                    }
+                }
+                System.out.println(remainingRelations + " " + totalTime);
+            }
+        }
+        int pp=0;
+        pp+=1;
+    }
+
+}
