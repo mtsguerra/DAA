f95491d747e150e80cc8cbb12e0f206d68f43ce7
mtsguerra7 <mtsguerra9@gmail.com>
2025-03-20 22:54:20 +0000

    better version of it

--- lista2/exG2.java
+++ lista2/exG2.java
@@ -8,10 +8,12 @@ class exG2{
         int numberOfNodes = input.nextInt();
         int numberOfActivities = input.nextInt();
         int k = input.nextInt();
+
         // time spent to do all the activities
         int totalTime = 0;
-
+        // remaining edges for each node
         int[] nEdges = new int[numberOfNodes+1];
+        // connections between nodes
         HashMap<Integer, List<int[]>> branches = new HashMap<>();
         for (int i = 1; i <= numberOfNodes; i++) {
             branches.put(i, new ArrayList<>());
@@ -25,8 +27,6 @@ class exG2{
             branches.get(node1).add(new int[]{node2,weight});
             nEdges[node2]++;
         }
-        int n=2;
-        n+=2;
         KahnAlgorithm(branches,nEdges,numberOfActivities,numberOfNodes, totalTime, k);
     }
 
@@ -34,21 +34,23 @@ class exG2{
                                       int [] nEdges, int numberOfActivities, int numberOfNodes,
                                        int totalTime, int k){
         Queue<Integer> queue = new LinkedList<>();
-        int index =0;
-        for (int zz =0;zz<numberOfActivities;zz++){
-            for (int i=1;i<=numberOfNodes;i++){
-                if (nEdges[i] == 0){
-                    queue.add(i);
-                    nEdges[i] = -1;
-                }
+        // initial topological order (excluding k)
+        for (int i = 1; i <= numberOfNodes; i++) {
+            if (nEdges[i] == 0 && i != k) {
+                queue.add(i);
+                nEdges[i] = -1;
             }
-            queue.remove(k);
-            while (!queue.isEmpty()){
-                int current = queue.poll();
-                index++;
-                for (int[] neighbor : connections.get(current)){
-                    nEdges[neighbor[0]]--;
-                    totalTime -= neighbor[1];
+        }
+        while (!queue.isEmpty()){
+            int current = queue.poll();
+            for (int[] neighbor : connections.get(current)){
+                nEdges[neighbor[0]]--;
+                // decreases the time spent by the weight of the edge
+                totalTime -= neighbor[1];
+                // add newly available nodes (excluding k)
+                if (nEdges[neighbor[0]] == 0 && neighbor[0] != k) {
+                    queue.add(neighbor[0]);
+                    nEdges[neighbor[0]] = -1;
                 }
             }
         }
