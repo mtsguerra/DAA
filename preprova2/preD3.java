6f17d4ce7f4c68a6008c70e48804e372a4ed0b7e
Matheus Goncalves Guerra <mtsguerra9@gmail.com>
2025-05-20 17:47:32 +0100

    Update preD3.java

--- preprova2/preD3.java
+++ preprova2/preD3.java
@@ -1,9 +1,11 @@
 import java.util.*;
 
 class Edgess {
+    int from;
     int neighbor;
     int capacity;
-    Edgess(int neighbor, int capacity) {
+    Edgess( int from, int neighbor, int capacity) {
+        this.from = from;
         this.neighbor = neighbor;
         this.capacity = capacity;
     }
@@ -16,7 +18,11 @@ class preD3{
     public static void main(String[] args) {
         int nNodes = input.nextInt();
         int nEdges = input.nextInt();
+
+        @SuppressWarnings("unchecked")
         List<Edgess> [] graph = new ArrayList[nNodes+1];
+        List<Edgess> edges = new ArrayList<>();
+
         for (int i=1;i<=nNodes;i++){
             graph[i] = new ArrayList<>();
         }
@@ -25,21 +31,21 @@ class preD3{
             int node1 = input.nextInt();
             int node2 = input.nextInt();
             int capacity = input.nextInt();
-            graph[node1].add(new Edgess(node2,capacity));
+            Edgess e = new Edgess(node1, node2, capacity);
+            edges.add(e);
+            graph[node1].add(e);
             leadTo[node2]++;
         }
 
-        List<Integer> topologicalOrder = new ArrayList<>();
         Queue<Integer> queue = new LinkedList<>();
         int[] earliestTime = new int[nNodes+1];
-        for (int i=0;i<leadTo.length;i++){
+        for (int i=1;i<leadTo.length;i++){
             if (leadTo[i] == 0){
                 queue.add(i);
             }
         }
         while (!queue.isEmpty()){
             int current = queue.poll();
-            topologicalOrder.add(current);
             for (Edgess edge : graph[current]){
                 if (earliestTime[edge.neighbor]  < earliestTime[current] + edge.capacity){
                     earliestTime[edge.neighbor] = earliestTime[current] + edge.capacity;
@@ -51,10 +57,27 @@ class preD3{
             }
         }
 
-        for (int i=1;i<=nNodes;i++){
-            System.out.print(earliestTime[i] + " ");
+        TreeMap<Integer, Integer> map = new TreeMap<>();
+        for (Edgess edge : edges){
+            int start = earliestTime[edge.from];
+            int end = start + edge.capacity;
+            map.put(start, map.getOrDefault(start, 0) + 1);
+            map.put(end, map.getOrDefault(end, 0) - 1);
         }
 
+        int running =0;
+        int maxRunning = 0;
+        int firstTime = 0;
+        for (Map.Entry<Integer, Integer> entry : map.entrySet()){
+            int time = entry.getKey();
+            int delta = entry.getValue();
+            running += delta;
+            if (running > maxRunning){
+                maxRunning = running;
+                firstTime = time;
+            }
+        }
+        System.out.println(earliestTime[nNodes] + " " + maxRunning + " " + firstTime);
     }
 
 }
