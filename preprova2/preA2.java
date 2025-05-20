7ab94580e667803e0604aa9808edfe08eb7b4436
Matheus Goncalves Guerra <mtsguerra9@gmail.com>
2025-05-20 23:47:17 +0100

    Add files via upload

---
+++ preprova2/preA2.java
@@ -0,0 +1,28 @@
+import java.util.*;
+
+public class preA2 {
+
+    private static final Scanner input = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int nNodes = input.nextInt();
+        int nConnections = input.nextInt();
+        HashSet<Integer> [] connections = new HashSet[nNodes+1];
+        for (int i=1;i<=nNodes;i++){
+            connections[i] = new HashSet<>();
+        }
+        for (int i = 0; i < nConnections; i++) {
+            int sz = input.nextInt();
+            int initial = input.nextInt();
+            for (int j = 0; j < sz-1; j++) {
+                int next = input.nextInt();
+                connections[initial].add(next);
+                initial = next;
+            }
+        }
+        for (int i=1;i<=nNodes;i++){
+            System.out.println(connections[i].size());
+        }
+    }
+
+}
