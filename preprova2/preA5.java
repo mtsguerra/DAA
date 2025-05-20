603d407f1b9fa5b3aa540955ea1b9b4bcffda679
Matheus Goncalves Guerra <mtsguerra9@gmail.com>
2025-05-20 20:39:48 +0100

    Update preA5.java

--- preprova2/preA5.java
+++ preprova2/preA5.java
@@ -1,12 +1,10 @@
 import java.util.*;
 
 class edededg{
-    int from;
-    int to;
+    int node;
     int places;
-    public edededg(int from, int to, int places){
-        this.from = from;
-        this.to = to;
+    public edededg(int node, int places){
+        this.node = node;
         this.places = places;
     }
 }
@@ -24,11 +22,50 @@ public class preA5 {
         int nEdges = input.nextInt();
 
         @SuppressWarnings("unchecked")
-        List<edededg> [] graph = new ArrayList[nNodes];
+        List<edededg> [] graph = new ArrayList[nNodes+1];
 
         for (int i = 1; i <= nNodes; i++) {
             graph[i] = new ArrayList<>();
         }
+
+        for (int i = 0; i < nEdges; i++) {
+            int node1 = input.nextInt();
+            int node2 = input.nextInt();
+            int avaiableSeats = input.nextInt();
+            graph[node1].add(new edededg(node2, avaiableSeats));
+        }
+
+        int [] possibles = possibles(graph, origin, nNodes, maxElements, minElements);
+        boolean none = true;
+        for (int i=1;i<possibles.length;i++){
+            if (i==origin || possibles[i] < minElements) continue;
+            none = false;
+            System.out.println("Destino " + i + ": " + possibles[i]);
+        }
+        if (none) System.out.println("Impossivel");
+
+    }
+
+    private static int[] possibles (List<edededg> [] graph, int from, int nNodes, int maxElements, int minElements) {
+        int[] possibles = new int[nNodes+1];
+        Arrays.fill(possibles, Integer.MIN_VALUE);
+        possibles[from] = maxElements;
+
+        PriorityQueue<edededg> pq = new PriorityQueue<>(Comparator.comparingInt((edededg e) -> e.places).reversed());
+        pq.offer(new edededg(from, maxElements));
+
+        while (!pq.isEmpty()) {
+            edededg current = pq.poll();
+            for (edededg neighbour : graph[current.node]) {
+                int newPlaces = Math.min(current.places, neighbour.places);
+                if (newPlaces < minElements) continue;
+                if (newPlaces > possibles[neighbour.node]) {
+                    possibles[neighbour.node] = newPlaces;
+                    pq.offer(new edededg(neighbour.node, newPlaces));
+                }
+            }
+        }
+        return possibles;
     }
 
 }
